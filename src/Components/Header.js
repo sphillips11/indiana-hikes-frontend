8c0991104a0a8d2a540719cebc24a5aaaba3d94c
Shay <shay.phillips11@gmail.com>
2022-02-13 02:10:24 -0500

    updated header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import "./Header.css";
 
 const Header = (props) => {
   return (
@@ -8,10 +9,31 @@ const Header = (props) => {
         <Navbar.Brand href="/">Indiana Hikes</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto justify-content-end">
-            <Nav.Link href="/">Home</Nav.Link>
+          <Nav className="ms-auto align-items-center">
             <Nav.Link href="/signup">Sign Up</Nav.Link>
             <Nav.Link href="/login">Login</Nav.Link>
+            <NavDropdown
+              title={
+                <span>
+                  <img
+                    className="icon"
+                    src="images/hiker_icon.png"
+                    alt="Hiker icon"
+                  />{" "}
+                  "hiker.name"
+                </span>
+              }
+              id="basic-nav-dropdown"
+            >
+              <NavDropdown.Item href="/hikers/{hiker.id}">
+                Profile
+              </NavDropdown.Item>
+              <NavDropdown.Item href="/hikers/{hiker.id}/hikes">
+                Hikes
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
+            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </Container>
